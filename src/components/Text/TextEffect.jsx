aa915035806a3e8c2072cc8e78ea4c5662c66d89
g.soni <g.soni@caddlance.com>
2024-06-25 18:45:35 +0530

    Text effect section set

--- src/components/Text/TextEffect.jsx
+++ src/components/Text/TextEffect.jsx
@@ -13,7 +13,7 @@ const TextEffect = () => {
       const scrollDistance = window.scrollY;
       const updatedSpans = initialSpans.map((span, index) => ({
         ...span,
-        active: scrollDistance  >= index * 2.7
+        active: scrollDistance  >= index * 2.75
       }));
       setSpans(updatedSpans);
     };
